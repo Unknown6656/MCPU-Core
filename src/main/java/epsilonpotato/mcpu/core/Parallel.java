2d703e8aee1414458b9e392b0131b983e99dc711
Unknown6656 <unknown84@gmx.de>
2017-10-18 07:10:56 +0200

    added parallel class

---
+++ src/main/java/epsilonpotato/mcpu/core/Parallel.java
@@ -0,0 +1,84 @@
+package epsilonpotato.mcpu.core;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Consumer;
+
+
+public class Parallel
+{
+    static final int nCPU = Runtime.getRuntime().availableProcessors();
+    
+    
+    public static <T> void ForEach(Iterable<T> parameters, final Consumer<T> loopBody)
+    {
+        ExecutorService executor = Executors.newFixedThreadPool(nCPU);
+        List<Future<?>> futures = new LinkedList<Future<?>>();
+        
+        for (final T param : parameters)
+        {
+            Future<?> future = executor.submit(new Runnable()
+            {
+                public void run()
+                {
+                    loopBody.accept(param);
+                }
+            });
+            
+            futures.add(future);
+        }
+        
+        for (Future<?> f : futures)
+            try
+            {
+                f.get();
+            }
+            catch (InterruptedException e)
+            {
+            }
+            catch (ExecutionException e)
+            {
+            }
+        
+        executor.shutdown();
+    }
+    
+    public static void For(int start, int stop, final Consumer<Integer> loopBody)
+    {
+        ExecutorService executor = Executors.newFixedThreadPool(nCPU);
+        List<Future<?>> futures = new LinkedList<Future<?>>();
+        
+        for (int i = start; i < stop; i++)
+        {
+            final Integer k = i;
+            
+            Future<?> future = executor.submit(new Runnable()
+            {
+                public void run()
+                {
+                    loopBody.accept(k);
+                }
+            });
+            
+            futures.add(future);
+        }
+        
+        for (Future<?> f : futures)
+            try
+            {
+                f.get();
+            }
+            catch (InterruptedException e)
+            {
+            }
+            catch (ExecutionException e)
+            {
+            }
+        
+        executor.shutdown();
+    }
+}
